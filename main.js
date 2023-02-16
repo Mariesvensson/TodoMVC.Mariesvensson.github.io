c2dbac77c8f7a671e21fe218987c2d90b38b6152
Mariesvensson <112252345+Mariesvensson@users.noreply.github.com>
2023-02-16 21:15:24 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,15 @@
+let input = document.querySelector('#user-input')
+
+function submitInput(event) {
+
+    if (event.keyCode === 13){
+
+        event.preventDefault();
+
+
+    }
+}
+function markAllButton(){
+
+
+}
